69f01ca36d48d68bd3629d21c6805d29bbe29196
Mika Savela <59084144+msavela1@users.noreply.github.com>
2026-05-08 11:52:46 +0300

    Update import paths for gosmi packages

--- module.go
+++ module.go
@@ -3,9 +3,9 @@ package gosmi
 import (
 	"fmt"
 
-	"github.com/sleepinggenius2/gosmi/models"
-	"github.com/sleepinggenius2/gosmi/smi"
-	"github.com/sleepinggenius2/gosmi/types"
+	"github.com/basen/gosmi/models"
+	"github.com/basen/gosmi/smi"
+	"github.com/basen/gosmi/types"
 )
 
 type SmiModule struct {
